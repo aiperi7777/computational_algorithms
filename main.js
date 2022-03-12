d7531db99b170e1ab67bcfc230b592d809c72b53
aiperi <uwuowo733@gmail.com>
2022-03-12 22:56:02 +0300

    find sinx

--- main.js
+++ main.js
@@ -1,12 +1,28 @@
-let u = 1;
+let sum = 0;
 
-function test(s, k, res) {
+function testExponenta(s, k, res) {
     let newRes;
-    if (Math.abs(res) > 0, 00000001) {
+    if (Math.abs(res) > 0.00000001) {
         console.log(res)
+        sum += res
         newRes = (s / k) * res
         k++
         test(s, k, newRes)
-    } else return
+    }
+    console.log("sum=", Number(sum.toFixed(8)))
 }
-test(1 / 4, 1, 1)
+// test(1 / 4, 1, 1)
+let sumSin = 0
+
+function testSin(x, k, res) {
+    let newRes;
+    if (Math.abs(res) > 0.0001) {
+        sumSin += res;
+        newRes = (-(x * x) / (2 * k * (2 * k + 1))) * res
+        k++
+        console.log(newRes.toFixed(4))
+        testSin(x, k, newRes)
+    }
+    console.log("sumSin=", Number(sumSin.toFixed(4)))
+}
+// testSin(0.41714, 1, 0.41714)
