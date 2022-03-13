d957a4b59ff7e16d62c162b1fe573a13421650d7
aiperi <uwuowo733@gmail.com>
2022-03-14 00:07:52 +0300

    нахождение многочлена по схеме горнера

--- main.js
+++ main.js
@@ -14,15 +14,29 @@ function testExponenta(s, k, res) {
 // test(1 / 4, 1, 1)
 let sumSin = 0
 
-function testSin(x, k, res) {
+function sinx(x, k, res) {
     let newRes;
     if (Math.abs(res) > 0.0001) {
         sumSin += res;
         newRes = (-(x * x) / (2 * k * (2 * k + 1))) * res
         k++
         console.log(newRes.toFixed(4))
-        testSin(x, k, newRes)
+        sinx(x, k, newRes)
     }
     console.log("sumSin=", Number(sumSin.toFixed(4)))
 }
-// testSin(0.41714, 1, 0.41714)
+// sinx(0.41714, 1, 0.41714)
+
+
+function getRt() {}
+
+function getPt(x, a, n, k) {
+
+    if (k + 1 < n) {
+        return getPt(x, a, n, k + 1) * x + a[k]
+    } else return a[n] * x + a[k]
+}
+let res = getPt(2, [1, 2, 3, 4], 3, 0)
+console.log(res)
+
+function getQmt() {}
