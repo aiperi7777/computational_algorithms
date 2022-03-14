dac84a7bb55ace1655ea7f0c12336f7a0b84c8a6
aiperi <uwuowo733@gmail.com>
2022-03-14 23:13:00 +0300

    add

--- main.js
+++ main.js
@@ -1,43 +1,81 @@
+let testInps = document.getElementById('test');
+let testInps2 = document.getElementById('test1');
+let getN = document.getElementById('getN');
+let inpChisl = document.getElementById('inp_koefV');
+let inpZnam = document.getElementById('inp_koefN');
+//--------------- вычисление экспоненты --------------------------------
+// встроенные методы
+let e = Math.exp(1 / 4);
+//  console.log(e.toFixed(8)); //1.28402542
+// с помощью формул
 let sum = 0;
-
-function testExponenta(s, k, res) {
+let k = 1;
+let res = 1; //u0
+function exp(s, k, res) {
     let newRes;
+
     if (Math.abs(res) > 0.00000001) {
-        console.log(res)
-        sum += res
-        newRes = (s / k) * res
-        k++
-        testExponenta(s, k, newRes)
+        //  console.log(res.toFixed(8));
+        sum += res;
+        newRes = (s / k) * res;
+        k++;
+        exp(s, k, newRes);
     }
-    console.log("sum=", Number(sum.toFixed(8)))
+    return sum.toFixed(8);
 }
-testExponenta(1 / 4, 1, 1)
+// console.log('e^(1/4) = ', exp(0.25, k, res));
+//----------------- вычисление синуса --------------------
+// встроенные методы
+let x = document.getElementById('inp_x').value;
+// let x = 0.41714;
+// let sin = Math.sin(x);
+let result = x;
+//  console.log(sin.toFixed(8)); //0.40514735
+//  с помощью формул
+let sumSin = 0;
 
-let sumSin = 0
-
-function sin(x, k, res) {
+function findSin(x, k, result) {
     let newRes;
-    if (Math.abs(res) > 0.0001) {
-        sumSin += res;
-        newRes = (-(x * x) / (2 * k * (2 * k + 1))) * res
-        k++
-        console.log(newRes.toFixed(4))
-        sin(x, k, newRes)
+    if (Math.abs(result) > 0.0001) {
+        sumSin += result;
+        newRes = (-(x * x) / (2 * k * (2 * k + 1))) * result;
+        k++;
+        //  console.log(newRes.toFixed(5));
+        findSin(x, k, newRes);
     }
-    console.log("sumSin=", Number(sumSin.toFixed(4)))
+    return sumSin.toFixed(4);
 }
-// sinx(0.41714, 1, 0.41714)
-
-
-function getRt() {
-    console.log(getPt(2, [1, 2, 3, 4], 3, 0) / getPt(1, [1, 1, 1, 1], 3, 0))
-}
-getRt()
+console.log('sin 0.41714 = ', findSin(x, k, result));
+// пользователь вводит массив элементов, коэффициенты не целые числа, проверки на ввод корректных данных желательно,
+findRes.addEventListener('click', () => {
+    let x = document.getElementById('inp_x').value;
+    let result = x;
+    findSin(x, k, result);
+    document.getElementById('res_e').value = exp(0.25, k, res);
+});
 
 function getPt(x, a, n, k) {
     if (k + 1 < n) {
-        return getPt(x, a, n, k + 1) * x + a[k]
-    } else return a[n] * x + a[k]
+        return getPt(x, a, n, k + 1) * x + a[k];
+    }
+    return a[n] * x + a[k];
 }
-let res = getPt(6, [2, -9, -20, -57], 3, 0)
-console.log(res)
+
+let res1 = getPt(2, [2, 3, 1, 4, 13], 4, 0);
+
+console.log(res1);
+getN.addEventListener('click', () => {
+    generateInps(inpChisl.value, testInps);
+    generateInps(inpZnam.value, testInps2);
+});
+
+function generateInps(n, tagId) {
+    tagId.innerHTML = '';
+    for (let i = 0; i < n; i++) {
+        let newElem = document.createElement('input');
+        newElem.type = 'number';
+        newElem.className = `inp${i}${tagId}`;
+        newElem.className = 'test-style';
+        tagId.appendChild(newElem);
+    }
+}
